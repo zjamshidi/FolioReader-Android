75054f163e814d924952787b535dcd47293b729d
Zahra Jamshidi <zahra@parsida.se>
2020-07-03 12:24:11 +0530

    [CRASH] Proguard is removing ReadLocator attributes

--- folioreader/src/main/java/com/folioreader/model/locators/ReadLocator.kt
+++ folioreader/src/main/java/com/folioreader/model/locators/ReadLocator.kt
@@ -3,6 +3,7 @@ package com.folioreader.model.locators
 import android.os.Parcel
 import android.os.Parcelable
 import android.util.Log
+import androidx.annotation.Keep
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
@@ -14,6 +15,7 @@ import org.readium.r2.shared.LocatorText
 
 @JsonPropertyOrder("bookId", "href", "created", "locations")
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Keep
 open class ReadLocator : Locator, Parcelable {
 
     var bookId: String
